b1cbb57d4ff4a32c984835625fe04b36acdf63e3
taomee517 <taomee517@qq.com>
2019-04-24 21:02:09 +0800

    去掉webjars依赖，加入中英文切换

--- src/main/java/com/demo/springboot/config/WebMvcConfig.java
+++ src/main/java/com/demo/springboot/config/WebMvcConfig.java
@@ -1,10 +1,16 @@
 package com.demo.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 /**
  * WebMvc配置，指定登录页访问路径
@@ -18,8 +24,33 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("login_origin");
-        registry.addViewController("/index.html").setViewName("login_origin");
+        registry.addViewController("/").setViewName("login_temp");
+        registry.addViewController("/index.html").setViewName("login_temp");
     }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
+    public class MyLocaleResolver implements LocaleResolver{
+        @Override
+        public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+            String l = httpServletRequest.getParameter("l");
+            Locale locale = Locale.getDefault();
+            if(!StringUtils.isEmpty(l)){
+                String[] localInfo = l.split("_");
+                //locale[0] 语言信息 locale[1] 国家信息
+                locale = new Locale(localInfo[0],localInfo[1]);
+            }
+            return locale;
+        }
+
+        @Override
+        public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+        }
+    }
+
 
 }
